b41d5edd9dadaa9d85d18eb1993c94512a8234b3
Apna-project2003 <214593185+Apna-project2003@users.noreply.github.com>
2025-07-01 18:18:21 +0530

    Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+// import React, { useContext } from 'react'
+// import{Route,Routes} from 'react-router-dom'
+// import ApplyJob from './pages/ApplyJob'
+// import Applications from './pages/Applications'
+// import Home from './pages/Home'
+// import Navbar from './components/Navbar'
+
+// import { AppContext } from './context/AppContext'
+// import RecruiterLogin from './components/RecruiterLogin'
+// import Dashboard from './pages/Dashboard'
+// import AddJobs from './pages/AddJobs'
+// import MangeJobs from './pages/MangeJobs'
+// import ViewApplications from './pages/ViewApplications'
+// import 'quill/dist/quill.snow.css'
+// const App = () => {
+//   const{showRecruiterLogin} = useContext(AppContext)
+//   return (
+//   <div>
+//    {showRecruiterLogin &&  <RecruiterLogin/>}
+//     <Routes>
+//       <Route path='/'element={<Home/>}></Route>
+//       <Route path='/apply-job/:id' element={<ApplyJob/>} />
+//       <Route path='/Applications'element={<Applications/>} />
+//       <Route path="/dashboard" element ={<Dashboard/>} >
+//        <Route path="add-jobs" element ={<AddJobs/>}/>
+//         <Route path='manage-jobs' element ={<MangeJobs/>} />
+
+//          <Route path='view-applications' element ={<ViewApplications/>} />
+//        </Route>
+//     </Routes>
+
+//   </div>
+
+//   )
+// }
+
+// export default App
+
+
 import React, { useContext } from 'react'
-import{Route,Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import ApplyJob from './pages/ApplyJob'
 import Applications from './pages/Applications'
 import Home from './pages/Home'
@@ -11,25 +50,28 @@ import Dashboard from './pages/Dashboard'
 import AddJobs from './pages/AddJobs'
 import MangeJobs from './pages/MangeJobs'
 import ViewApplications from './pages/ViewApplications'
+import 'quill/dist/quill.snow.css'
+
 const App = () => {
-  const{showRecruiterLogin} = useContext(AppContext)
+  const { showRecruiterLogin } = useContext(AppContext)
+
   return (
-  <div>
-   {showRecruiterLogin &&  <RecruiterLogin/>}
-    <Routes>
-      <Route path='/'element={<Home/>}></Route>
-      <Route path='/apply-job/:id' element={<ApplyJob/>} />
-      <Route path='/Applications'element={<Applications/>} />
-      <Route path='/dashboard' element ={<Dashboard/>} />
-       <Route path='/add-job' element ={<AddJobs/>} />
-        <Route path='/manage-jobs' element ={<MangeJobs/>} />
+    <div>
+      {showRecruiterLogin && <RecruiterLogin />}
 
-         <Route path='/view-applications' element ={<ViewApplications/>} />
-       
-    </Routes>
-
-  </div>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Applications />} />
+        
+        {/* ✅ Nested routes under /dashboard */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-jobs" element={<AddJobs />} />
+          <Route path="manage-jobs" element={<MangeJobs />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
