446b03a168ff59cde427a4be99f17816adaaee66
Apna-project2003 <214593185+Apna-project2003@users.noreply.github.com>
2025-07-03 19:24:07 +0530

    Backend

--- Backend/routes/companyRoutes.js
+++ Backend/routes/companyRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ChangeJobApplicationsStatus, changeVisiblity, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controllers/companyControllers.js';
 import upload from '../config/multer.js';
+import { protectCompany } from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 
@@ -15,32 +16,32 @@ router.post('/login',loginCompany)
 
 //Get company data
 
-router.get('/company' , getCompanyData)
+router.get('/company' ,protectCompany ,getCompanyData)
 
 //Post a job
 
-router.post('/post-job',postJob)
+router.post('/post-job',protectCompany,postJob)
 
 
 
 // Get Applicants Data of Company
 
-router.get('/applicants',getCompanyJobApplicants)
+router.get('/applicants',protectCompany,getCompanyJobApplicants)
 
 
 // Get Company Job List
 
-router.get('/list-jobs',getCompanyPostedJobs)
+router.get('/list-jobs',protectCompany,getCompanyPostedJobs)
 
 
 //Change Applications Status
 
-router.post('/change-status',ChangeJobApplicationsStatus)
+router.post('/change-status',protectCompany,ChangeJobApplicationsStatus)
 
 
 // change Applications Visiblity
 
-router.post('/change-visiblity',changeVisiblity )
+router.post('/change-visiblity',protectCompany,changeVisiblity )
 
 
 
---
+++ Backend/routes/jobRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const router = express.Router()
+
+//Route to get all jobs data
+
+
+
+//Route to get a single job bu Id
+
+
+export default router
